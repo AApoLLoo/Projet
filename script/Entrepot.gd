d8cbc4f99a44a6efd1025c8e0f27513878863ffb
Louis-Regnault <louis.regnault-loquet@epfedu.fr>
2026-06-03 10:31:17 +0200

    Update Entrepot.gd
    Enregistrement entrepôt entre les parties

--- script/Entrepot.gd
+++ script/Entrepot.gd
@@ -1,16 +1,29 @@
-extends Entity 
+extends Entity
 class_name WarehouseEntity
 
-var inventory: Dictionary = {} 
+var inventory: Dictionary = {}
 
-# NE PAS REDÉCLARER LE SIGNAL ICI, IL EST DÉJÀ DANS ENTITY
-# signal entity_updated(entity)  <-- SUPPRIME CETTE LIGNE
-
-func _ready():
+func _ready() -> void:
+	entity_type = "entrepot"
 	add_to_group("entrepot")
+	super._ready()
 
 func add_resource(item_name: String, amount: int):
 	inventory[item_name] = inventory.get(item_name, 0) + amount
 	
 	# Tu utilises directement le signal hérité de Entity
 	entity_updated.emit(self)
+
+func serialize() -> Dictionary:
+	var data: Dictionary = super.serialize()
+	data["inventory"] = inventory.duplicate(true)
+	return data
+
+func deserialize(data: Dictionary) -> void:
+	super.deserialize(data)
+	var saved_inventory: Variant = data.get("inventory", {})
+	if saved_inventory is Dictionary:
+		inventory = saved_inventory.duplicate(true)
+	else:
+		inventory = {}
+	add_to_group("entrepot")
